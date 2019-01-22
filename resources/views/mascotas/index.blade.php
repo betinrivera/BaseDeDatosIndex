a46b407342100f25659e14a846d4577ef96f0030
Alumno <ulsa@varela.local>
2019-01-21 18:53:48 -0700

    BD

---
+++ resources/views/mascotas/index.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lista de mascotas</title>
+</head>
+<body>
+    <button>Agregar Mascota</button>
+    <table>
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            <tr>
+        </thead>
+        <tbody>
+            @foreach($mascotas as $mascota)
+            <tr>
+                <td>{{ $mascota->nombre }}</td>
+                <td>{{ $mascota->precio }}</td>
+                <td>
+                    <button>Editar</button>
+                    <button>Borrar</button>
+                </td>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+</html>
